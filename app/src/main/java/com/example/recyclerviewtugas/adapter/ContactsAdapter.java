e8d4d67a24c8505a7afc4798f4a441e1e8ac5a01
febrianthiintan <febrianthii@gmail.com>
2019-02-26 20:57:16 +0700

    RecyclerView Tugas

--- app/src/main/java/com/example/recyclerviewtugas/adapter/ContactsAdapter.java
+++ app/src/main/java/com/example/recyclerviewtugas/adapter/ContactsAdapter.java
@@ -1,4 +1,4 @@
-package com.example.recyclereview.adapter;
+package com.example.recyclerviewtugas.adapter;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
--- app/src/main/java/com/example/recyclerviewtugas/adapter/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewtugas/adapter/MainActivity.java
@@ -1,11 +1,11 @@
-package com.example.recyclereview;
+package com.example.recyclerviewtugas.adapter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
-import com.example.recyclereview.adapter.ContactsAdapter;
+import com.example.recyclereview.R;
 import com.example.recyclereview.models.Contact;
 
 import java.util.ArrayList;
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
         RecyclerView rv = findViewById(R.id.rvContact);
         contacts = Contact.createContactsList(10);
-        ContactsAdapter contactsAdapter = new ContactsAdapter(contacts);
+        com.example.recyclereview.adapter.ContactsAdapter contactsAdapter = new com.example.recyclereview.adapter.ContactsAdapter(contacts);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         rv.setAdapter(contactsAdapter);
         rv.setLayoutManager(layoutManager);
