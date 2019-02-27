9963a9df229a9175d4bccc2e0dde3c0797a106d4
febrianthiintan <febrianthii@gmail.com>
2019-02-27 22:38:27 +0700

    RecyclerView Tugas

---
+++ app/src/main/java/datakontak/contactname.java
@@ -0,0 +1,21 @@
+package datakontak;
+
+public class contactname {
+    private String nama;
+    private String asal;
+    private String nohp;
+
+    public contactname(String nama, String asal, String nohp) {
+        this.nama = nama;
+        this.asal = asal;
+        this.nohp = nohp;
+    }
+
+    public String getNama(){
+        return nama;
+    }
+
+    public String getNama(){
+        return nama;
+    }
+}
